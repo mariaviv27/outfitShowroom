f35d75c285dbbd4e8924f4168647d022259a1185
mariaviv27 <maria.vivar@opendeusto.es>
2024-10-28 13:27:13 +0100

    admin

--- appOutfitShowroom/admin.py
+++ appOutfitShowroom/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Estilo, Ocasion, Outfit
+admin.site.register(Estilo)
+admin.site.register(Ocasion)
+admin.site.register(Outfit)
