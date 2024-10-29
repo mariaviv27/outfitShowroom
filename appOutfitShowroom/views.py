180e9acf2d1974095cb089b242e402c0b2aac922
AneAltunaa <ane.altuna@opendeusto.es>
2024-10-29 13:28:18 +0100

    Se ve la pagina pero sin estilos

--- appOutfitShowroom/views.py
+++ appOutfitShowroom/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404
 from django.http import HttpResponse
+from .models import Estilo, Ocasion, Outfit
 
 def index(request):
-    return HttpResponse("Listado de outfits")
+    #return HttpResponse("Listado de outfits")
+	return render(request, 'nocobot/index.html')
+	
